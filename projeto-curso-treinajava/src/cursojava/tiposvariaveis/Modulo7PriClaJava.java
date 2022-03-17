f57a7f39df4916382608650bdfccae001c433743
ivanferreira-java <ivan.ir.cjava@gmail.com>
2022-03-17 12:41:59 -0300

    Criação do pacote tiposvariaveis e da classe Modulo7PriClaJava

---
+++ projeto-curso-treinajava/src/cursojava/tiposvariaveis/Modulo7PriClaJava.java
@@ -0,0 +1,32 @@
+package cursojava.tiposvariaveis;
+
+public class Modulo7PriClaJava {
+	
+	/*MAIN é um metodo auto executavel em JAVA*/
+	public static void main(String[] args) {
+		/*
+		boolean logica;
+		boolean logica2;
+		
+		int numero;
+		int numero2;
+		
+		short numero4;
+		*/
+		int maiorIdade = 18;
+		int idosoIdade = 60;
+		int jovenIdade = 16;
+		int criancaIdade = 7;
+		
+		String cpf = "123.456.789-00";
+		
+		System.out.println("Maior idade é: " + maiorIdade);
+		System.out.println("Idoso é: " + idosoIdade);
+		System.out.println("Jovem idade é: " + jovenIdade);
+		System.out.println("Criança idade é: " + criancaIdade);
+		System.out.println("CPF é: " + cpf);
+		
+		
+	}
+
+}
